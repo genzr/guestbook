20bfed2138c690288188428585cab3c94ae66df5
unknown <genzr05@gmail.com>
2022-08-27 14:52:06 +1000

    first ever t3 stack app

---
+++ src/pages/index.tsx
@@ -0,0 +1,111 @@
+import { signIn, useSession, signOut } from "next-auth/react";
+import { useState } from "react";
+import { trpc } from "../utils/trpc";
+
+const Messages = () => {
+  const { data: messages, isLoading } = trpc.useQuery(["guestbook.getAll"]);
+
+  if (isLoading) return <div>Fetching messages...</div>;
+
+  return (
+    <div className="flex flex-col gap-4">
+      {messages?.map((msg, index) => {
+        return (
+          <div key={index}>
+            <p>{msg.message}</p>
+            <span>- {msg.name}</span>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+const Home = () => {
+
+  const { data: session, status } = useSession();
+  const [message, setMessage] = useState("");
+
+  const ctx = trpc.useContext();
+
+  const postMessage = trpc.useMutation("guestbook.postMessage", {
+    onMutate: () => {
+      ctx.cancelQuery(["guestbook.getAll"]);
+
+      let optimisticUpdate = ctx.getQueryData(["guestbook.getAll"]);
+
+      if(optimisticUpdate) {
+        ctx.setQueryData(["guestbook.getAll"], optimisticUpdate);
+      }
+
+    },
+    onSettled: () => {
+
+      ctx.invalidateQueries(["guestbook.getAll"]);
+
+    }
+  });
+
+  if (status === "loading") {
+    return <main>Loading...</main>;
+  }
+
+  return (
+    <main className="flex flex-col items-center align-middle">
+
+      <h1 className="text-3xl pt-4">Guestbook</h1>
+
+      {session ? (
+        <div className="pt-10">
+          <p>hi {session.user?.name}</p>
+          <button onClick={() => signOut()}>Logout</button>
+
+          <div className="pt-6">
+              <form
+                className="flex gap-2"
+                onSubmit={(event) => {
+                  event.preventDefault();
+
+                  postMessage.mutate({
+                    name: session.user?.name as string,
+                    message,
+                  });
+
+                  setMessage("");
+                }}
+              >
+                <input
+                  type="text"
+                  value={message}
+                  placeholder="Your message..."
+                  maxLength={100}
+                  onChange={(event) => setMessage(event.target.value)}
+                  className="px-4 py-2 rounded-md border-2 border-zinc-800 bg-neutral-900 focus:outline-none"
+                />
+                <button
+                  type="submit"
+                  className="p-2 rounded-md border-2 border-zinc-800 focus:outline-none"
+                >
+                  Submit
+                </button>
+              </form>
+            </div>
+
+          <div className="pt-10">
+            <Messages />
+          </div>
+        </div>
+      ) : (
+        <div>
+          <button onClick={() => signIn("discord")}>Login with Discord</button>
+          <div className="pt-10">
+          <Messages />
+          </div>
+        </div>
+      )}
+
+    </main>
+  );
+};
+
+export default Home;
---
+++ src/server/router/guestbook.ts
@@ -0,0 +1,45 @@
+import { z } from "zod";
+import { createRouter } from "./context";
+
+export const guestbookRouter = createRouter()
+.query("getAll", {
+  async resolve({ ctx }) {
+    try {
+      return await ctx.prisma.guestbook.findMany({
+        select: {
+          name: true,
+          message: true,
+        },
+        orderBy: {
+          createdAt: "desc",
+        },
+      });
+    } catch (error) {
+      console.log("error", error);
+    }
+  },
+})
+.middleware(async ({ ctx, next }) => {
+    if (!ctx.session) {
+        throw new Error("Not authenticated");
+        }
+        return next();
+    })
+.mutation("postMessage", {
+  input: z.object({
+    name: z.string(),
+    message: z.string(),
+  }),
+  async resolve({ ctx, input }) {
+    try {
+      await ctx.prisma.guestbook.create({
+        data: {
+          name: input.name,
+          message: input.message,
+        },
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  },
+});
